55cdf489e37c59d989a3ae5d12d6e8c03ce557db
hanisntsolo <ds.pratap1997@hotmail.com>
2024-02-09 03:35:00 +0530

    feat: env mapping added:test3

--- set-env.sh
+++ set-env.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 echo "Environment Coming from build step :: $ENV_TYPE"
 
+ENV_DIR=/app/env
+BASE_DIR=/app
+
 if [ "$ENV_TYPE" = "dev" ]; then
-  cp /env/.env.dev .env
+  cp $ENV_DIR/.env.dev $BASE_DIR/.env
 elif [ "$ENV_TYPE" = "uat" ]; then
-  cp /env/.env.uat .env
+  cp $ENV_DIR/.env.uat $BASE_DIR/.env
 elif [ "$ENV_TYPE" = "prod" ]; then
-  cp /env/.env.prod .env
+  cp $ENV_DIR/.env.prod $BASE_DIR/.env
 else
-  echo "Unknown environment"
+  echo "Unknown environment :: $ENV_TYPE"
   exit 1
 fi
 
