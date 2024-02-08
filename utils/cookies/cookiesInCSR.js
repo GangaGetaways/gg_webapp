b3bb3e4f374412863c309d376f9de56b4f0bb421
Anisha Gupta <anisha.gupta@infoedge.com>
2024-02-08 13:19:10 +0530

    build error fixed...............

--- utils/cookies/cookiesInCSR.js
+++ utils/cookies/cookiesInCSR.js
@@ -1,28 +1,32 @@
 export function getCookieInCSR(name="") {
-    const cookieArr = document.cookie.split(";");
+    try{
+        const cookieArr = document.cookie.split(";");
    
-    for(let i = 0; i < cookieArr.length; i++) {
-        const cookiePair = cookieArr[i].split("=");
-        if(name == cookiePair[0].trim()) {
-            // Decode the cookie value and return
-            return decodeURIComponent(cookiePair[1]);
+        for(let i = 0; i < cookieArr.length; i++) {
+            const cookiePair = cookieArr[i].split("=");
+            if(name == cookiePair[0].trim()) {
+                // Decode the cookie value and return
+                return decodeURIComponent(cookiePair[1]);
+            }
         }
-    }
-    
-    // Return empty string if not found
-    return "";
+        
+        // Return empty string if not found
+        return "";
+    }catch(err){}
 }
 
 export const setCookieInCSR = (name, value, expires, path, domain, secure) => {
-    const today = new Date();
-    today.setTime(today.getTime());
-    if (expires) {
-        expires = expires * 1000 * 60 * 60 * 24;
-    }
-    const expires_date = new Date(today.getTime() + (expires));
-    document.cookie = name + '=' + encodeURI(value) +
-        ((expires) ? ';expires=' + expires_date.toGMTString() : '') +
-        ((path) ? ';path=' + path : '') +
-        ((domain) ? ';domain=' + domain : '') +
-        ((secure) ? ';secure' : '');
+    try{
+        const today = new Date();
+        today.setTime(today.getTime());
+        if (expires) {
+            expires = expires * 1000 * 60 * 60 * 24;
+        }
+        const expires_date = new Date(today.getTime() + (expires));
+        document.cookie = name + '=' + encodeURI(value) +
+            ((expires) ? ';expires=' + expires_date.toGMTString() : '') +
+            ((path) ? ';path=' + path : '') +
+            ((domain) ? ';domain=' + domain : '') +
+            ((secure) ? ';secure' : '');
+    }catch(err){}
 }
